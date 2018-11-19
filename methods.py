5d67a53cf9ff4f61a41dc98fac9ff85dcf2f4a9c
Courtney <campbell.1807@osu.edu>
2018-11-19 00:53:00 -0500

    update get total words method

--- methods.py
+++ methods.py
@@ -1,5 +1,15 @@
 # this method takes in a .txt file and returns the number of words in the file
 def get_total_number_of_words(file):
+    wordCount = 0
+    f = open(file, "r")
+    # read first line
+    line = f.readline()
+    while line:
+        # do whatever
+        line = f.readline()
+        wordCount = wordCount + 1
+    f.close()
+    return wordCount
 
 # getTotalUniqueWords()
 # this method returns the number of unique words in the novel
